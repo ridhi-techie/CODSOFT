bae5f3d01afd000b717e3fffea40978df076fc76
Ridhi Puri <ridhipuri23@gmail.com>
2025-07-24 17:41:06 +0530

    Delete job-board directory

--- job-board/client/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-// ✅ Import the axios package from node_modules
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend base URL
-  withCredentials: true, // optional
-});
-
-export default instance;
